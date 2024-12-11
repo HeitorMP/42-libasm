49664ef2d0cec0f41a9c7eea7084b5481d4537d9
heitormp <heitormp@protonmail.com>
2024-12-11 11:16:28 +0000

    fix: errno in ft_write

--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -1,26 +1,21 @@
-
-;	ssize_t		ft_write(int fd, const void *buf, size_t count);
-;	rdi = fd
-;	rsi = buf
-;	rdx = count
-
 section .text
     global ft_write
     extern __errno_location
 
 ft_write:
 
-    mov rax, 1                          ; syscall write
+    mov rax, 0x1                        ; syscall write
     syscall                             ; params already in order, calling syscall
 
     cmp rax, 0                          ; checks if return of rax with 0
     jge .return                         ; if rax >= 0, no error goto return
 
-    ;error handler
-    neg rax
-    mov rdi, rax                        ; rdi = rax(erro code)
-    call __errno_location WRT ..plt     ; call errno_location external function, errno now is set but rax is changed to
-    mov rax, -1
+    ; error handler
+    neg rax                             ; save the error code (negative value)
+    mov rdi, rax
+    call __errno_location WRT ..plt     ; call errno_location external function
+    mov [rax], rdi
+    mov rax, -1                         ; set return value to -1 (error indicator)
 
 .return:
     ret
